d45a736333e1ec69775343c3b2a1aefea67b0f91
kmerry <kjmerry15@gmail.com>
2018-12-01 11:05:02 -0700

    Removed unnecessary typespec

--- src/day1.kt
+++ src/day1.kt
@@ -22,7 +22,7 @@ fun day1p2(changeList: List<String>) : Int {
 }
 
 fun main(args: Array<String>) {
-    val examples: Array<String> = arrayOf("+1, +1, +1", "+1, +1, -2", "-1, -2, -3")
+    val examples = arrayOf("+1, +1, +1", "+1, +1, -2", "-1, -2, -3")
     for (inp in examples) {
         println("$inp -> ${day1p1(inp.split(", "))}")
     }
@@ -31,7 +31,7 @@ fun main(args: Array<String>) {
     File("test/day1p1.txt").forEachLine { ct += it.toInt() }
     println("day1p1.txt -> $ct")
 
-    val examplesPart2: Array<String> = arrayOf("+1, -1", "+3, +3, +4, -2, -4", "-6, +3, +8, +5, -6", "+7, +7, -2, -7, -4")
+    val examplesPart2 = arrayOf("+1, -1", "+3, +3, +4, -2, -4", "-6, +3, +8, +5, -6", "+7, +7, -2, -7, -4")
     for (changeList in examplesPart2) {
         println("$changeList -> ${day1p2(changeList.split(", "))}")
     }
